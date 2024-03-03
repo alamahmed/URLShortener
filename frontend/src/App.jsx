040a419d754b2adec1f14cba7d859cea7019aa49
Ahmed Alam <ahmedalam@Ahmeds-MBP.fritz.box>
2024-03-03 16:42:30 +0100

    Create frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,43 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <>
-    </>
-  );
+    <div className='container displayFlex'>
+      <h1 className='main-heading'>
+        URL Shortner
+      </h1>
+      <div className='sub-container displayFlex'>
+        <h1 className='heading'>
+          Paste Your Link Here
+        </h1>
+        <form
+          action='../backend/main.py'
+          className='form'
+        >
+          <section className='input-container displayFlex'>
+            <div className='input-sub-container'>
+              <input
+                className='input'
+                type='text'
+                id='fname'
+                placeholder='Your Link'
+                name='fname'
+              />
+              <button
+                onSubmit={() => {
+
+                }}
+                className='button border'
+              >
+                Shorten URL
+              </button>
+            </div>
+          </section>
+        </form>
+      </div>
+    </div >
+  )
 }
 
 export default App;
