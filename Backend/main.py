83e147ec52784c5586403ffb5b6c5d3b033d5e01
Ahmed Alam <ahmedalam@Ahmeds-MBP.fritz.box>
2024-03-04 11:48:53 +0100

    Connect Database

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,63 @@
+# To activate venv
+# source myenv/bin/activate
+# To Run python server locally with reload on change
+# uvicorn main:app --reload
 import hashlib
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
-def hashGen(url):
-    test = hashlib.md5(url.encode('utf-8'))
-    print("hash value: ", test.hexdigest())
+# Use a service account.
+cred = credentials.Certificate( './urlshortner.json' )
+app = firebase_admin.initialize_app( cred )
+db = firestore.client()
+app = FastAPI()
 
-hashGen("https://github.com/new")
+doc_ref = db.collection( 'URLShortner' ).document( 'URL' )
+
+# Function to generate HashURL
+def hashGen( url ):
+    hashedURL = hashlib.md5( url.encode( 'utf-8' ) )
+    hashedURL = hashedURL.hexdigest()
+    return hashedURL
+
+# Remove CORS Error
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3000",
+]
+
+# Allow origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = [ "*" ],
+    allow_headers = [ "*" ],
+)
+
+# Allow to pass string
+class URL( BaseModel ):
+    url:str
+
+# Create post request
+@app.post( '/' )
+async def uploadItem( URL: URL ):
+    url = URL.url
+    hashedURL = hashGen( url )
+    # Add url in firebase database
+    doc_ref.set( { hashedURL: url }, merge = True )
+    # Return message and shortenedURL
+    return { 'Message': 'Success', 'URL': hashedURL }
+
+@app.get( '/{hash}')
+async def redirect( hash:str ):
+    data = doc_ref.get()
+    response = RedirectResponse( url = data.to_dict()[ hash ] )
+    return response
