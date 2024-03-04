36f27c2939130e9190924bac37aab44447a95f9a
Ahmed Alam <ahmedalam@Ahmeds-MBP.fritz.box>
2024-03-04 20:17:17 +0100

    allow all origins

--- Backend/main.py
+++ Backend/main.py
@@ -25,20 +25,12 @@ def hashGen( url ):
     hashedURL = hashedURL.hexdigest()
     return hashedURL
 
-# Remove CORS Error
-origins = [
-    'https://alamahmed.github.io/URLShortner/',
-    'http://localhost',
-    'http://localhost:5500',
-    'http://0.0.0.0',
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
