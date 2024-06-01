b4fee20b05b357f29a83af0bc234c25fd050c307
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Air.fritz.box>
2024-06-01 15:03:31 +0200

    update allow origin

--- Backend/main.py
+++ Backend/main.py
@@ -22,9 +22,14 @@ def hashGen( url ):
     hashedURL = hashedURL.hexdigest()
     return hashedURL
 
+origin = [
+    'http://localhost:3000/',
+    'https://url-shortener-neon-two.vercel.app/'
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origin,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
